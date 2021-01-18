d562dedc6bde3734c06e441e044839d76ef4ef96
Leon_2802 <leon.gobbert@hs-furtwangen.de>
2021-01-18 16:01:09 +0100

    new tileset

---
+++ 2D-RPG new try/Assets/collisionManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionManager : MonoBehaviour
+{
+        public Rigidbody2D rb;
+        public bool isMoving = true;
+    private void OnCollisionEnter2D(Collision2D collide) 
+    {
+            if(collide.collider.CompareTag("Player"))
+            {
+                isMoving = false;
+                rb.mass = 100000;
+            }
+    }
+
+    private void OnCollisionExit2D(Collision2D collide) 
+    {
+        if(collide.collider.CompareTag("Player"))
+        {
+            isMoving = true;
+            rb.mass = 1;
+        }
+    }
+
+    private void Move() 
+    {
+        if(isMoving == false)
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
