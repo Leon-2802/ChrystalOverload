22646701d2724a410917b20d489969d086a66dfe
Leon_2802 <leon.gobbert@hs-furtwangen.de>
2021-01-21 11:45:48 +0100

    enemy spawner added

---
+++ 2D-RPG new try/Assets/enemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public Transform target;
+    public GameObject enemyPrefab;
+    public bool spawning = true;
+    public float respawnTime;
+
+    void Update()
+    {
+        if(spawning == true)
+        {
+           StartCoroutine(spawnTimer());
+        }
+    }
+
+    private void spawnEnemy()
+    {
+        GameObject enemy = Instantiate(enemyPrefab) as GameObject;
+        enemy.transform.position = new Vector2 (9, 1);
+    }
+    private IEnumerator spawnTimer()
+    {
+        spawning = false;
+        yield return new WaitForSeconds(respawnTime);
+        spawnEnemy();
+        spawning = true;
+    }
+
+}
